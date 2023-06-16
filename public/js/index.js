50933c3c0926424ca3712b5fc31fba72d4a8d882
Le Duc Thien <timducle@yahoo.com>
2023-06-16 17:09:34 +0700

    0.4

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,51 @@
 window.onload = () => {
   console.log('Page loaded');
   let signupEl = document.getElementById('signup');
+  let loginEl = document.getElementById('login');
+  let logoutEl = document.getElementById('logout');
+  let userEl = document.getElementById('user');
+  const user = netlifyIdentity.currentUser();
+  
+  if(user) {
+    loginEl.hidden = true;
+    signupEl.hidden = true;
+    logoutEl.hidden = false;
+    userEl.hidden = false;
+    userEl.innerHTML = `<span>Logged in as ${user.user_metadata.full_name}</span>`;
+    console.log(JSON.stringify(user));
+  } else {
+    logoutEl.hidden = true;
+    userEl.hidden = true;
+    loginEl.hidden = false;
+    signupEl.hidden = false;
+  }
+
+  netlifyIdentity.on('login', user => {    
+    loginEl.hidden = true;
+    signupEl.hidden = true;
+    logoutEl.hidden = false;
+    userEl.hidden = false;
+    userEl.innerHTML = `<span>Logged in as ${user.user_metadata.full_name}</span>`;
+    
+  });
+
+  netlifyIdentity.on('logout', user => {
+    logoutEl.hidden = true;
+    userEl.hidden = true;
+    loginEl.hidden = false;
+    signupEl.hidden = false;
+  });
+  
   signupEl.addEventListener('click', () => {
-    console.log('Signup clicked');
+    netlifyIdentity.open();
+  });
+
+  loginEl.addEventListener('click', () => {
+    netlifyIdentity.open();
+  });
+
+  logoutEl.addEventListener('click', () => {
+    netlifyIdentity.logout();
   });
 };
 
